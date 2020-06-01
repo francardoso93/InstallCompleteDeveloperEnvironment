a81c5839df42fde2496704f1a0cb6f31dac1636a
Francisco Cardoso <francardoso@outlook.com>
2020-06-01 09:14:36 -0300

    First working script for restoring my environment after format

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Install Complete Developer Environment
+
+This script installs my most used programs/packages/resources for daily work
+
+Read script before executing, so you can remove what you won't 
+use.
---
+++ restoredeveloperenv.sh
@@ -0,0 +1,24 @@
+#Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update ; sudo apt-get install google-chrome-stable -y
+#VsCode
+sudo snap install --classic code # or code-insiders
+#GIT
+sudo apt-add-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git -y
+git --version
+git config --global user.name "Francisco Cardoso"
+git config --global user.email "francardoso@outlook.com"
+#SSH
+ssh-keygen
+#Node
+sudo apt-get install curl -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs -y
+node -v
+npm -v
+#Nest and Angular CLIs
+sudo npm i -g @nestjs/cli
+sudo npm install -g @angular/cli
