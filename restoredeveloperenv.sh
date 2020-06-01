c1433a605b36ff83c8cedf15cb8fa9f231a3922c
Francisco Cardoso <francardoso@outlook.com>
2020-06-01 17:04:51 -0300

    Atualizando versao do docker-compose

--- restoredeveloperenv.sh
+++ restoredeveloperenv.sh
@@ -33,7 +33,7 @@ sudo apt-get update
 sudo apt-get install docker-ce -y
 sudo usermod -aG docker $USER
 # Docker-Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 # OpenShot (Video Editor)
