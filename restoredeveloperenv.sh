6b5a377c3563f233a2304b703e65e2a403e0725f
Francisco Cardoso <francardoso@outlook.com>
2020-07-02 07:37:10 -0300

    2 versoes dotnet

--- restoredeveloperenv.sh
+++ restoredeveloperenv.sh
@@ -51,10 +51,10 @@
 # # DotNet
 # wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 # sudo dpkg -i packages-microsoft-prod.deb
-# sudo apt-get update; \
-#   sudo apt-get install -y apt-transport-https && \
-#   sudo apt-get update && \
-# sudo apt-get install -y dotnet-sdk-2.2
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+sudo apt-get install -y dotnet-sdk-2.2
 # sudo apt-get install -y dotnet-sdk-3.1
 # # Peek (Gif Recorder)
 # sudo add-apt-repository ppa:peek-developers/stable
