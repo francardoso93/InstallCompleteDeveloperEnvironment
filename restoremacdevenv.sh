080b1d936b4f26214c5bc4ca5748caeb88f24ecf
Francisco Cardoso <francisco.cardoso@Franciscos-Mac-mini.local>
2023-07-11 16:12:28 -0300

    bundler, xcode and node

--- restoremacdevenv.sh
+++ restoremacdevenv.sh
@@ -1,4 +1,4 @@
-# TOOD: brewfile
+# TODO: brewfile
 # brew (wich also install xcode command line tools)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/francisco.cardoso/.zprofile
@@ -11,7 +11,15 @@ brew install --cask google-chrome
 brew install --cask visual-studio-code
 # slack
 brew install --cask slacks
+# Ruby Bundler
+sudo gem install bundler:2.4.13
 # mas (allows installing apps from the appstore)
 brew install mas
-# xcode
+# xcode (Make sure you're properly logged in the App Store before running this)
 mas install 497799835
+# xcode-select (Required by fastlane)
+sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+# nodejs
+brew install node
+# yarn
+npm install -g yarn
