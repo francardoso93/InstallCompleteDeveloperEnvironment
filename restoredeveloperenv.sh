4311455813db496ebd95aee68c635e8beb02caa7
Francisco Cardoso <francardoso@outlook.com>
2024-12-09 11:19:25 -0300

    update kind

--- restoredeveloperenv.sh
+++ restoredeveloperenv.sh
@@ -34,7 +34,7 @@
 # sudo mv ./kubectl /usr/local/bin/kubectl
 # kubectl version --client
 # # Kind (Lightweight Kubernetes)
-# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-amd64
 # chmod +x ./kind
 # sudo mv ./kind /usr/local/bin/kind
 # # Helm (Kub package manager)
