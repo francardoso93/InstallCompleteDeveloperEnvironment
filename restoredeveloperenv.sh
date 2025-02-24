12243cf498c4f029fe0c630b868262a903f573f4
Francisco Cardoso <francardoso@outlook.com>
2025-02-24 17:28:35 -0300

    ready for new laptop

--- restoredeveloperenv.sh
+++ restoredeveloperenv.sh
@@ -1,77 +1,77 @@
 # #GIT
-# #sudo apt-add-repository ppa:git-core/ppa
-# #sudo apt-get update
-# #sudo apt-get install git -y
-# #git --version
-# git config --global user.name "Francisco Cardoso"
-# git config --global user.email "francardoso@outlook.com"
-# # Google Chrome
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo dpkg -i google-chrome-stable_current_amd64.deb
-# #VsCode
-# sudo snap install --classic code # or code-insiders#
-# echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-# echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p # https://github.com/flathub/com.visualstudio.code/issues/29
-# #SSH
-# ssh-keygen
+sudo apt-add-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git -y
+git --version
+git config --global user.name "Francisco Cardoso"
+git config --global user.email "francardoso@outlook.com"
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+#VsCode
+sudo snap install --classic code # or code-insiders#
+echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p # https://github.com/flathub/com.visualstudio.code/issues/29
+#SSH
+ssh-keygen
 # #Postman
 # sudo snap install postman
-# # Docker # https://tecadmin.net/install-docker-on-ubuntu/
-# sudo apt-get purge docker-ce docker lxc-docker docker-engine docker.io
-# sudo apt-get install curl apt-transport-https ca-certificates software-properties-common -y
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# sudo apt-get update
-# sudo apt-get install docker-ce -y
-# sudo usermod -aG docker $USER
-# # Docker-Compose
-# sudo curl -L https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
-# docker-compose --version
+# Docker # https://tecadmin.net/install-docker-on-ubuntu/
+sudo apt-get purge docker-ce docker lxc-docker docker-engine docker.io
+sudo apt-get install curl apt-transport-https ca-certificates software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo usermod -aG docker $USER
+# Docker-Compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 # # Kubectl
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-# chmod +x ./kubectl
-# sudo mv ./kubectl /usr/local/bin/kubectl
-# kubectl version --client
-# # Kind (Lightweight Kubernetes)
-# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-amd64
-# chmod +x ./kind
-# sudo mv ./kind /usr/local/bin/kind
-# # Helm (Kub package manager)
-# sudo snap install helm --classic
-# helm plugin install https://github.com/databus23/helm-diff
-# # Add Helm Rabbit Repository # helm install --kubeconfig=/home/francisco/.kube/k3s.yaml rabbit bitnami/rabbitmq --namespace logistics
-# #helm repo add bitnami https://charts.bitnami.com/bitnami
-# #helm repo update
-# #echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-# # ZSH
-# sudo apt install zsh -y
-# sudo apt-get install powerline fonts-powerline -y
-# git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-# cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-# chsh -s /bin/zsh
-# # Curl
-# sudo apt install curl -y
-# # Build Essentials
-# sudo apt-get install build-essential
-# # ASDF
-# git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-# cd ~/.asdf
-# git checkout "$(git describe --abbrev=0 --tags)" # Latest version branch
-# cd ~
-# echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-# echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-# echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
-# echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-# source $HOME/.asdf/asdf.sh
-# source ~/.bashrc
-# asdf --version
-# # ASDF Node
-# asdf plugin-add nodejs
-# bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
-# asdf install nodejs latest
-# asdf global nodejs latest
-# node -v
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+# Kind (Lightweight Kubernetes)
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+# Helm (Kub package manager)
+sudo snap install helm --classic
+helm plugin install https://github.com/databus23/helm-diff
+# Add Helm Rabbit Repository # helm install --kubeconfig=/home/francisco/.kube/k3s.yaml rabbit bitnami/rabbitmq --namespace logistics
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm repo update
+#echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+# ZSH
+sudo apt install zsh -y
+sudo apt-get install powerline fonts-powerline -y
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+# Curl
+sudo apt install curl -y
+# Build Essentials
+sudo apt-get install build-essential
+# ASDF
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+cd ~/.asdf
+git checkout "$(git describe --abbrev=0 --tags)" # Latest version branch
+cd ~
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+source $HOME/.asdf/asdf.sh
+source ~/.bashrc
+asdf --version
+# ASDF Node
+asdf plugin-add nodejs
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+asdf install nodejs latest
+asdf global nodejs latest
+node -v
 # # # ASDF Java
 # # asdf plugin-add java https://github.com/halcyon/asdf-java.git
 # # asdf install java latest:adoptopenjdk-17
@@ -79,34 +79,34 @@
 # # echo -e '\n. $HOME/.asdf/plugins/java/set-java-home.zsh' >> ~/.zshrc
 # # echo -e '\n. $HOME/.asdf/plugins/java/set-java-home.bash' >> ~/.bashrc
 # # ASDF Go
-# asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-# asdf install golang latest
-# asdf global golang latest
-# echo -e '\nexport GOROOT="$(asdf where golang)/go/"' >> ~/.zshrc
-# echo -e '\nexport CC="gcc"' >> ~/.zshrc
-# # Ruby
-# # sudo apt-get install ruby-full -y
-# # sudo gem install bundler
-# # Terraform
-# asdf plugin-add terraform
-# asdf install terraform latest
-# # Pulumi
-# curl -fsSL https://get.pulumi.com | sh
-# # Tweaks
-# sudo add-apt-repository universe
-# sudo apt install gnome-tweak-tool -y
-# # Slack
-# sudo snap install slack --classic
-# # Zoom
-# sudo snap install zoom-client
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf install golang latest
+asdf global golang latest
+echo -e '\nexport GOROOT="$(asdf where golang)/go/"' >> ~/.zshrc
+echo -e '\nexport CC="gcc"' >> ~/.zshrc
+# Ruby
+# sudo apt-get install ruby-full -y
+# sudo gem install bundler
+# Terraform
+asdf plugin-add terraform
+asdf install terraform latest
+# Pulumi
+curl -fsSL https://get.pulumi.com | sh
+# Tweaks
+sudo add-apt-repository universe
+sudo apt install gnome-tweak-tool -y
+# Slack
+sudo snap install slack --classic
+# Zoom
+sudo snap install zoom-client
 # # Discord
 # sudo snap install discord
 # #psql
-# sudo apt-get install -y postgresql-client
+sudo apt-get install -y postgresql-client
 # # AWS CLI
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 # # Isolate Workspaces
 # gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
 # # Peek (Gif Recorder) # Can't find this on Ubuntu 22
@@ -125,48 +125,47 @@
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/francisco/.profile
 # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-# # pgcli 
-# sudo apt-get install pgcli
-# # k8s krew
-# (
-#   set -x; cd "$(mktemp -d)" &&
-#   OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-#   ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-#   KREW="krew-${OS}_${ARCH}" &&
-#   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-#   tar zxvf "${KREW}.tar.gz" &&
-#   ./"${KREW}" install krew
-# )
-# echo -e '\nexport PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.zshrc
-# echo -e '\nexport PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.bashrc
-# source ~/.bashrc
+# pgcli 
+sudo apt-get install pgcli
+# k8s krew
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+echo -e '\nexport PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.zshrc
+echo -e '\nexport PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 # stern
-#  kubectl krew install stern
+kubectl krew install stern
 # # kubens
-# kubectl krew install ns
-# Kube PS1 (to show current context and ns) need to be done manually, but easy. See: https://github.com/jonmosco/kube-ps1sudo apt install fzf
+kubectl krew install ns
+# Kube PS1 (to show current context and ns) need to be done manually, but easy. See: https://github.com/jonmosco/kube-ps1sudo 
 # k6
 # sudo gpg -k
 # sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
 # echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
 # sudo apt-get update
 # sudo apt-get install k6
-# # Warp CLI
-# sudo apt-get install wget gpg -y
-# wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
-# sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
-# sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
-# rm warpdotdev.gpg
-# sudo apt update && sudo apt install warp-terminal -y
-# sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/warp-terminal 100 # Default Terminal
+# Warp CLI
+sudo apt-get install wget gpg -y
+wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
+sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
+rm warpdotdev.gpg
+sudo apt update && sudo apt install warp-terminal -y
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/warp-terminal 100 # Default Terminal
 # # Falcon Sensor (Get '.deb' file from Slack conversation with Felix T. Bellemare and Martin Bruno on slack. Get CID from the same conversation)
 # sudo dpkg -i Downloads/falcon-sensor_7.14.0-16703_amd64.deb
 # sudo /opt/CrowdStrike/falconctl -s --cid=<CCID>
-# Remember to manually update crontab to start and stop the service at work hours
-# # Cloudflare warp. More details on this doc: https://sonder.freshservice.com/support/solutions/articles/16000081760
-# curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ bookworm main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-# sudo apt update && sudo apt install cloudflare-warp -y
+# Cloudflare warp. More details on this doc: https://sonder.freshservice.com/support/solutions/articles/16000081760
+curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ bookworm main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+sudo apt update && sudo apt install cloudflare-warp -y
 # Store git pass safetly (Clone private repos using 'https')
 # https://gist.github.com/sgarciav/b709c871fb040e3444e044642e3d8709
 sudo apt install pass
@@ -205,9 +204,9 @@ sudo dpkg -i ripgrep_14.1.0-1_amd64.deb
 # fd - user friendly 'find'
 sudo apt install fd-find
 # zsh history sync
-sudo apt install gpg git
-git clone git@github.com:wulfgarpro/history-sync.git
-cp -r history-sync ~/.oh-my-zsh/plugins
+# sudo apt install gpg git
+# git clone git@github.com:wulfgarpro/history-sync.git
+# cp -r history-sync ~/.oh-my-zsh/plugins
 # argocli
 curl -sLO https://github.com/argoproj/argo-workflows/releases/download/v3.5.11/argo-linux-amd64.gz
 gunzip argo-linux-amd64.gz
@@ -219,12 +218,12 @@ curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/lat
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 argocd version
-# Difftastic
-curl -sLO https://github.com/Wilfred/difftastic/releases/download/0.60.0/difft-x86_64-unknown-linux-gnu.tar.gz
-tar -xvzf difft-x86_64-unknown-linux-gnu.tar.gz
-chmod +x difft
-sudo mv ./difft /usr/local/bin/
-difft
+# # Difftastic
+# curl -sLO https://github.com/Wilfred/difftastic/releases/download/0.60.0/difft-x86_64-unknown-linux-gnu.tar.gz
+# tar -xvzf difft-x86_64-unknown-linux-gnu.tar.gz
+# chmod +x difft
+# sudo mv ./difft /usr/local/bin/
+# difft
 # Must Fix# Nushell
 # sudo apt install pkg-config libssl-dev build-essential
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -243,3 +242,13 @@ sudo apt -y install skopeo
 # download kubebuilder and install locally.
 curl -L -o kubebuilder "https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)"
 chmod +x kubebuilder && sudo mv kubebuilder /usr/local/bin/
+## TODO: New tools:
+# k9s
+wget https://github.com/derailed/k9s/releases/download/v0.40.5/k9s_linux_amd64.deb
+sudo apt install ./k9s_linux_amd64.deb -y
+rm k9s_linux_amd64.deb
+# Wireshark
+sudo add-apt-repository ppa:wireshark-dev/stable
+sudo apt-get update
+sudo apt-get install wireshark -y
+# TODO: silver-surfer (About cluster updates, let's test it)
